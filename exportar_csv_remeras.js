97f6f12c7385bfb15ef45303d6e2db464905a226
Ariel Bruschini <bruschini@gmail.com>
2022-04-16 16:01:22 -0300

    First Commit

---
+++ exportar_csv_remeras.js
@@ -0,0 +1,249 @@
+const importJSON = 
+        [
+            {"id":"46","codigo":"0046","codigo_ml_acbruschini":"MLA605758523","codigo_ml_enaniza":"MLA731537251","titulo":"Los Autos Locos - Pier Nodoyuna & Patan","subtitulo":"Remera de algodón estampada","texto":"Espectacular remera estampada de <strong>Pier Nodoyuna y Patán<\/strong> dos de los personajes más representativos de la serie infantil <strong>Wacky Races - Los Autos Locos<\/strong>.<br>Todas nuestras remeras son de suave algodón Velvet Cotton 24\/1 de primera calidad y se encuentran estampadas con vinilos termotransferibles importados que perduran en el tiempo y son resistentes a los lavados.  ","arte_frente":"0046_wacky_races_pier_sintesis\/arte_remera.jpg","arte_render":"0046_wacky_races_pier_sintesis\/render_remera.jpg","seccion":"Series Animadas","tags":"wacky|races|pier|autos|locos|cartoon|network","precio":"1890","color_principal":"Rojo","imagen_tienda":"0046re_rojo_producto.jpg","galeria_tienda":"0046re_rojo_hombre.jpg|0046re_rojo_mujer.jpg","categoria_tienda":"Indumentaria > Remeras 100% Algodon VC > Series Animadas","pictures_ml_acbruschini":"","pictures_ml_enaniza":"","terminado":"x"},
+            {"id":"47","codigo":"0047","codigo_ml_acbruschini":"MLA605758603","codigo_ml_enaniza":"MLA731537227","titulo":"Anatomy of a murder","subtitulo":"Remera de algodón estampada","texto":"Espectacular remera estampada de <strong>Anatomy of a murder<\/strong> cuyos títulos fueron realizados por el diseñador <strong>Saul Bass<\/strong>.<br>Todas nuestras remeras son de suave algodón Velvet Cotton 24\/1 de primera calidad y se encuentran estampadas con vinilos termotransferibles importados que perduran en el tiempo y son resistentes a los lavados.  ","arte_frente":"0047_saul_bass_anatomy_murder\/arte_remera.jpg","arte_render":"0047_saul_bass_anatomy_murder\/render_remera.jpg","seccion":"Cine","tags":"saul|bass|anatomy|murder|design|graphic|diseño","precio":"1890","color_principal":"Amarillo","imagen_tienda":"0047re_amarillo_producto.jpg","galeria_tienda":"0047re_amarillo_hombre.jpg|0047re_amarillo_mujer.jpg","categoria_tienda":"Indumentaria > Remeras 100% Algodon VC > Cine","pictures_ml_acbruschini":"","pictures_ml_enaniza":"","terminado":"x"},
+            {"id":"48","codigo":"0048","codigo_ml_acbruschini":"MLA605758530","codigo_ml_enaniza":"MLA731537214","titulo":"Bob Esponja - Face","subtitulo":"Remera de algodón estampada","texto":"Espectacular remera estampada del personaje <strong>Bob Esponja<\/strong> de la serie animada infantil del mismo nombre.<br>  En su espalda lleva el <strong>logo de la serie<\/strong>, también estampado.<br>Todas nuestras remeras son de suave algodón Velvet Cotton 24\/1 de primera calidad y se encuentran estampadas con vinilos termotransferibles importados que perduran en el tiempo y son resistentes a los lavados.  ","arte_frente":"0048_bob_esponja_sintesis_cara\/arte_remera.jpg","arte_render":"0048_bob_esponja_sintesis_cara\/render_remera.jpg","seccion":"Series Animadas","tags":"sponge|bob|square|pant|bob|esponja","precio":"1890","color_principal":"Amarillo","imagen_tienda":"0048re_amarillo_producto.jpg","galeria_tienda":"0048re_amarillo_hombre.jpg|0048re_amarillo_mujer.jpg","categoria_tienda":"Indumentaria > Remeras 100% Algodon VC > Series Animadas","pictures_ml_acbruschini":"","pictures_ml_enaniza":"","terminado":"x"},
+            {"id":"964","codigo":"0964","codigo_ml_acbruschini":"","codigo_ml_enaniza":"","titulo":"Sabrina - Spellman Mortuary","subtitulo":"Remera de algodón estampada","texto":"Espectacular remera estampada la serie <strong>Chilling Adventures of Sabrina<\/strong> basada en los comics del mismo nombre.<br>Todas nuestras remeras son de suave algodón Velvet Cotton 24\/1 de primera calidad y se encuentran estampadas con vinilos termotransferibles importados que perduran en el tiempo y son resistentes a los lavados.  ","arte_frente":"","arte_render":"","seccion":"Series y Tv","tags":"salem|spellman|baxter","precio":"1890","color_principal":"Negro","imagen_tienda":"0964re_negro_producto.jpg","galeria_tienda":"0964re_negro_hombre.jpg|0964re_negro_mujer.jpg","categoria_tienda":"","pictures_ml_acbruschini":"","pictures_ml_enaniza":"","terminado":""},
+            {"id":"953","codigo":"0953","codigo_ml_acbruschini":"","codigo_ml_enaniza":"","titulo":"Star Wars - Return of the Jedi","subtitulo":"Remera de algodón estampada","texto":"Espectacular remera estampada de la saga de películas <strong>Star Wars<\/strong>.<br>Todas nuestras remeras son de suave algodón Velvet Cotton 24\/1 de primera calidad y se encuentran estampadas con vinilos termotransferibles importados que perduran en el tiempo y son resistentes a los lavados.  ","arte_frente":"","arte_render":"","seccion":"Cine","tags":"star|wars|guerra|galaxias|george|rey|rogue|bb8|force|awakens|lucas|imperio|darth|vader|r2d2|c3po|anakin|leia|boba|fett|han|solo|sith|yoda|jedi|order|stormtrooper|death|star|luke|skywalker","precio":"1890","color_principal":"Negro","imagen_tienda":"0953re_negro_producto.jpg","galeria_tienda":"0953re_negro_hombre.jpg|0953re_negro_mujer.jpg","categoria_tienda":"Indumentaria > Remeras 100% Algodon VC > Cine","pictures_ml_acbruschini":"","pictures_ml_enaniza":"","terminado":""},
+            {"id":"10","codigo":"0002","codigo_ml_acbruschini":"MLA605759068","codigo_ml_enaniza":"MLA731537220","titulo":"Adventure Time - Jake","subtitulo":"Remera de algodón estampada","texto":"Espectacular remera estampada del personaje <strong>Jake<\/strong> de la serie animada <strong>Adventure Time. <\/strong>En su espalda lleva el <strong>logo de la serie<\/strong>, también estampado.<br>Todas nuestras remeras son de suave algodón Velvet Cotton 24\/1 de primera calidad y se encuentran estampadas con vinilos termotransferibles importados que perduran en el tiempo y son resistentes a los lavados.  ","arte_frente":"0002_adventure_time_jake\/arte_remera.jpg","arte_render":"0002_adventure_time_jake\/render_remera.jpg","seccion":"Series Animadas","tags":"finn|jake|cartoon|network|adventure","precio":"1890","color_principal":"Amarillo","imagen_tienda":"0002re_amarillo_producto.jpg","galeria_tienda":"0002re_amarillo_hombre.jpg|0002re_amarillo_mujer.jpg","categoria_tienda":"Indumentaria > Remeras 100% Algodon VC > Series Animadas","pictures_ml_acbruschini":"","pictures_ml_enaniza":"","terminado":"x"},
+            {"id":"11","codigo":"0003","codigo_ml_acbruschini":"MLA605759084","codigo_ml_enaniza":"MLA731537241","titulo":"Alf - Alfro","subtitulo":"Remera de algodón estampada","texto":"Espectacular remera estampada del personaje <strong>Alf<\/strong> de la exitosa serie de los 80 que lleva el mismo nombre. <\/strong>En su espalda lleva el <strong>logo de la serie<\/strong>, también estampado.<br>Todas nuestras remeras son de suave algodón Velvet Cotton 24\/1 de primera calidad y se encuentran estampadas con vinilos termotransferibles importados que perduran en el tiempo y son resistentes a los lavados.  ","arte_frente":"0003_alf_alfro\/arte_remera.jpg","arte_render":"0003_alf_alfro\/render_remera.jpg","seccion":"Series y Tv","tags":"willie|tanner|melmac|alf","precio":"1890","color_principal":"Marron","imagen_tienda":"0003re_marron_producto.jpg","galeria_tienda":"0003re_marron_hombre.jpg|0003re_marron_mujer.jpg","categoria_tienda":"Indumentaria > Remeras 100% Algodon VC > Series y Tv","pictures_ml_acbruschini":"","pictures_ml_enaniza":"","terminado":"x"},
+            {"id":"12","codigo":"0004","codigo_ml_acbruschini":"MLA605759089","codigo_ml_enaniza":"MLA731537235","titulo":"Alf - En forma de fichas","subtitulo":"Remera de algodón estampada","texto":"Espectacular remera estampada del personaje <strong>Alf<\/strong> de la exitosa serie de los 80 que lleva el mismo nombre. <\/strong>En su espalda lleva el <strong>logo de la serie<\/strong>, también estampado.<br>Todas nuestras remeras son de suave algodón Velvet Cotton 24\/1 de primera calidad y se encuentran estampadas con vinilos termotransferibles importados que perduran en el tiempo y son resistentes a los lavados.  ","arte_frente":"0004_alf_forma_de_fichas\/arte_remera.jpg","arte_render":"0004_alf_forma_de_fichas\/render_remera.jpg","seccion":"Series y Tv","tags":"willie|tanner|melmac|alf|fichas|simpsons|milhouse","precio":"1890","color_principal":"Negro","imagen_tienda":"0004re_negro_producto.jpg","galeria_tienda":"0004re_negro_hombre.jpg|0004re_negro_mujer.jpg","categoria_tienda":"Indumentaria > Remeras 100% Algodon VC > Series y Tv","pictures_ml_acbruschini":"","pictures_ml_enaniza":"","terminado":"x"},
+            {"id":"13","codigo":"0005","codigo_ml_acbruschini":"MLA609538413","codigo_ml_enaniza":"MLA731537230","titulo":"Amelie - Personaje","subtitulo":"Remera de algodón estampada","texto":"Espectacular remera estampada del personaje <strong>Amelie<\/strong> de la exitosa película francesa del mismo nombre. <\/strong>En su espalda lleva el <strong>logo del film<\/strong>, también estampado.<br>Todas nuestras remeras son de suave algodón Velvet Cotton 24\/1 de primera calidad y se encuentran estampadas con vinilos termotransferibles importados que perduran en el tiempo y son resistentes a los lavados.  ","arte_frente":"0005_amelie_sintesis_cara\/arte_remera.jpg","arte_render":"0005_amelie_sintesis_cara\/render_remera.jpg","seccion":"Cine","tags":"amelie|poulain|paris|audrey|tautou","precio":"1890","color_principal":"Blanco","imagen_tienda":"0005re_blanco_producto.jpg","galeria_tienda":"0005re_blanco_hombre.jpg|0005re_blanco_mujer.jpg","categoria_tienda":"Indumentaria > Remeras 100% Algodon VC > Cine","pictures_ml_acbruschini":"","pictures_ml_enaniza":"","terminado":"x"},
+            {"id":"14","codigo":"0006","codigo_ml_acbruschini":"MLA605759091","codigo_ml_enaniza":"MLA731537236","titulo":"Anteojos 3 dimensiones","subtitulo":"Remera de algodón estampada","texto":"Espectacular remera estampada con el gráfico de los clásicos <strong>anteojos 3d<\/strong> de nuestra tierna infancia.  <br>Todas nuestras remeras son de suave algodón Velvet Cotton 24\/1 de primera calidad y se encuentran estampadas con vinilos termotransferibles importados que perduran en el tiempo y son resistentes a los lavados.  ","arte_frente":"0006_anteojos_3d\/arte_remera.jpg","arte_render":"0006_anteojos_3d\/render_remera.jpg","seccion":"Gamer","tags":"lentes|gafas|rojo|cyan|tres","precio":"1890","color_principal":"Blanco","imagen_tienda":"0006re_blanco_producto.jpg","galeria_tienda":"0006re_blanco_hombre.jpg|0006re_blanco_mujer.jpg","categoria_tienda":"Indumentaria > Remeras 100% Algodon VC > Gamer","pictures_ml_acbruschini":"","pictures_ml_enaniza":"","terminado":"x"}
+        ]
+
+// ENTIDADES
+
+
+class Producto {
+    
+    constructor(id,codigo,seccion,nombre,precio,descripcion,tags)
+    {
+        if(this.constructor == Producto)
+        {
+            throw new Error("Producto es una clase abstracta y no puede ser instanciada")
+        }
+        this.id = parseInt(id);
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.tags = tags;
+        this.codigo = codigo;
+        this.seccion = seccion;
+    }
+    
+    getArrayTags() {
+        return this.tags.split("|");
+    }
+    
+    getTipoDeProducto() {
+        return this.constructor.name;
+    }
+
+    getTipoDeProductoPrefijo(){
+        let tipoResumido;
+        switch (this.getTipoDeProducto()) {
+            case "Remera":
+                tipoResumido = "re";
+                break;
+            case "Buzo":
+                tipoResumido = "bu";
+                    
+                break;            
+            case "Marco":
+                tipoResumido = "mc";
+                break;       
+            default:
+                break;
+        }
+        return tipoResumido;
+    }
+
+    getCodigoConTipo () { 
+        return this.getTipoDeProductoPrefijo() + "_" + this.codigo;
+    }
+
+}
+
+class Buzo extends Producto{
+    
+    constructor(nombre,precio,descripcion,colorPrincipal,talle){
+        super(nombre,precio,descripcion);
+        if(this.esColorPrincipalValido(colorPrincipal)){
+            this.colorPrincipal = colorPrincipal;
+        }
+        if(this.esTalleValido(talle)){
+            this.talle = talle.toUpperCase();
+        }
+    }
+    
+    toString () {
+        return `Buzo ${this.nombre} ($${this.precio}) cuyo color principal es ${this.colorPrincipal}`;
+    }
+    
+    esColorPrincipalValido(color){
+        const coloresPermitidos = ["Rojo","Negro","Azul","Blanco","Verde","Amarillo","Marron","Gris"];
+        return coloresPermitidos.includes(color);
+    }
+    
+    esTalleValido(talle){
+        const tallesPermitidos = ["S","M","L","XL","XXL"];
+        return tallesPermitidos.includes(talle.toUpperCase());
+    }
+    
+}
+
+class Remera extends Producto{
+    
+    static coloresPermitidosRemeras = ["Rojo","Negro","Azul","Blanco","Verde","Amarillo","Marron","Gris"];
+
+    constructor(id,codigo,seccion,nombre,precio,descripcion,colorPrincipal,tags,imagenPrincipal,imagenesSecundarias){
+        super(id,codigo,seccion,nombre,precio,descripcion,tags);
+        if(this.esColorPrincipalValido(colorPrincipal)){
+            this.colorPrincipal = colorPrincipal;
+        }
+
+        this.imagenPrincipal = imagenPrincipal;
+        this.imagenesSecundarias = imagenesSecundarias;
+
+    }
+    
+    getArrayImagenesSecundarias() {
+        return this.imagenesSecundarias.split("|");
+    }
+
+    getArrayImagenesEstampa(){
+        let tempArray = new Array();
+        Remera.coloresPermitidosRemeras.forEach(color => {
+            // tempArray.push()
+        })
+    }
+
+    getArrayImagenes() {
+        const tempArray = [this.imagenPrincipal];
+        return tempArray.concat(this.imagenesSecundarias.split("|"));
+    }
+
+    esColorPrincipalValido(color){
+        return Remera.coloresPermitidosRemeras.includes(color);
+    }
+    
+    static esTalleValido(talle,corte){
+        let retorno = false;
+        const cortesPermitidos = ["M","F","N"]; //Masculino, Femenino, Niños
+        const tallesPermitidosM = ["S","M","L","XL","XXL"];
+        const tallesPermitidosF = ["S","M","L"];
+        const tallesPermitidosN = ["6","8","10","12","14","16"];
+        if(cortesPermitidos.includes(corte.toUpperCase())){
+            switch (corte) {
+                case "M":
+                    if (tallesPermitidosM.includes(talle.toUpperCase()))
+                    {
+                        retorno = true;
+                    }
+                    break;
+                    case "F":
+                        if (tallesPermitidosF.includes(talle.toUpperCase()))
+                        {
+                            retorno = true;
+                        }
+                        break;
+                case "N":
+                    if (tallesPermitidosN.includes(talle.toUpperCase()))
+                    {
+                        retorno = true;
+                    }
+                    break;
+                }
+            } 
+            return retorno;
+    }
+
+    static arrayDeObjetosRemeras(arrayRemeras) {
+        const arrayReturn = new Array();
+        arrayRemeras.forEach(element => {
+            
+            arrayReturn.push(new Remera(element.id,element.codigo,element.seccion,element.titulo,element.precio,element.texto,element.color_principal,element.tags,element.imagen_tienda,element.galeria_tienda))
+            
+        });
+        return arrayReturn;
+    }
+
+    getLineaCsv (){
+        let descripcionWooCommerce = "\"[block id=\"\"descripcion-remera\"\"]\"";
+        let categoriaWooCommerce= "Remeras 100% Velvet Cotton|";
+        let direccionImgWooCommerce = "http://qualityserver.ddns.net:50001/img_tienda/ftp_re/";
+
+        return `${this.id};${this.getTipoDeProductoPrefijo()};${this.getTipoDeProductoPrefijo() + this.codigo};${this.nombre};${descripcionWooCommerce};${this.descripcion};${categoriaWooCommerce+this.seccion};${this.precio};`;
+    }
+
+}
+    
+class ExportRemeras {
+
+    static classNameTable = "mainContent-main-productsTable";
+    static classNameLine = "productLine";
+    static classNameId = "id";
+    static classNameMainImg = "mainImg";
+    static classNameSku = "sku";
+    static classNameTitle = "title";
+    static classNameDescription = "description";
+    static classNamePrice = "price";
+    static classNameImages = "images";
+    static classNameTags = "tags";
+
+    static createExportTable(arrayRemeras,direccionImg){
+        let table = document.createElement("div");
+        table.setAttribute("class",this.classNameTable);
+
+        arrayRemeras.forEach(element => {
+            let line = document.createElement("div");
+            line.setAttribute("class",`${this.classNameTable}-${this.classNameLine}`);
+
+            let direccionCompleta = direccionImg + element.getCodigoConTipo() + "/";
+            const arrayImgsSecundarias = element.getArrayImagenesSecundarias();
+            const arrayTags = element.getArrayTags();
+            
+            line.innerHTML += `<p class="${this.classNameTable}-${this.classNameLine}--${this.classNameId}">${element.id}</p>`;
+            line.innerHTML += `<div class="${this.classNameTable}-${this.classNameLine}--${this.classNameMainImg}">
+                                    <img src="${direccionCompleta}${element.imagenPrincipal}" alt="">
+                                </div>`
+            line.innerHTML += `<p class="${this.classNameTable}-${this.classNameLine}--${this.classNameSku}">${element.codigo}</p>`;
+            line.innerHTML += `<p class="${this.classNameTable}-${this.classNameLine}--${this.classNameTitle}">${element.nombre}</p>`;
+            line.innerHTML += `<p class="${this.classNameTable}-${this.classNameLine}--${this.classNameDescription}">${element.descripcion}</p>`;
+            line.innerHTML += `<p class="${this.classNameTable}-${this.classNameLine}--${this.classNamePrice}">${element.precio}</p>`;
+            
+            let divImg = document.createElement("div");
+            divImg.setAttribute("class",`${this.classNameTable}-${this.classNameLine}--${this.classNameImages}`);
+
+            arrayImgsSecundarias.forEach(imgs => {
+                divImg.innerHTML += `<img src="${direccionCompleta}${imgs}" alt="">`;
+            });
+
+            line.appendChild(divImg);
+            let tagsConEspacio = "";
+            arrayTags.forEach(tag => {
+                tagsConEspacio += " " + tag;
+            });
+
+            line.innerHTML += `<p class="${this.classNameTable}-${this.classNameLine}--${this.classNameTags}">${tagsConEspacio}</p>`;
+            table.appendChild(line);
+        });
+               
+        return table;
+    }
+
+}
+
+
+    // console.log(importJSON);
+    const $arrayProductos = Remera.arrayDeObjetosRemeras(importJSON);
+    //const $arrayProductosAExportar = new Array();
+    
+    // console.log($arrayProductos);
+    // console.log($arrayProductos[0].getArrayImagenes());
+    const tableContainer = document.getElementById("mainContent-main");
+    tableContainer.appendChild(ExportRemeras.createExportTable($arrayProductos,"http://qualityserver.ddns.net:50001/img_tienda/ftp_re/"));
+    // console.log(ExportRemeras.createExportTable($arrayProductos,"http://qualityserver.ddns.net:50001/img_tienda/ftp_re/"));
+
+    console.log($arrayProductos[0].getLineaCsv());
